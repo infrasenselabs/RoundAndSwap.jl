a712ae7d4013749b771df3dcfda4ec41495e42a4
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-08-06 09:18:54 +0100

    added a method to make a model for each thread

--- src/model.jl
+++ src/model.jl
@@ -1,6 +1,15 @@
 
 using JuMP
 
+using Ipopt, Gurobi, HiGHS
+
+solver_dict = Dict("Ipopt"=> Ipopt.Optimizer, "Gurobi"=> Gurobi.Optimizer, "HiGHS"=> HiGHS.Optimizer)
+
+"""Allow the getting of multiple variables"""
+function Base.getindex(m::JuMP.AbstractModel, names::Array{Symbol})
+    return [m[n] for n in names]
+end
+
 function successful(model::Model)
     return successful(termination_status(model))
 end
@@ -50,4 +59,16 @@ function fix!(models::Array{Model}, to_fix::Array{Symbol}, value =1 )
         fix(model[var], value, force=true)
     end
 end
+end
+
+function make_models(model::Model, multi_thread::Bool, optimizer::Union{Nothing, DataType}=nothing)
+    if !multi_thread
+        return [model]
+    end
+
+    optimizer = !isnothing(optimizer) ? optimizer : solver_dict[solver_name(model)]
+    _models = [copy(model) for _ in 1:Threads.nthreads()]
+    [MOI.set(_models[ii], MOI.Name(), "Model for thread: $ii") for ii in 1:Threads.nthreads()]
+    set_optimizer.(_models, optimizer)
+    return _models
 end
