479ffffc6c6dcee7e884ae4b612c3a8a4310ab14
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-08-07 12:03:10 +0100

    Sorted nice printing

--- src/printing.jl
+++ src/printing.jl
@@ -1,19 +1,21 @@
-using PrettyPrinting: best_fit, indent, list_layout, literal, pair_layout, pprint
+import Base.show
 
-# function _all_but_swapped(s::Swappable)
-#     ret_val = literal("Swappable:") * literal("\n") *
-#     indent(4) * literal("To swap      -> ") * list_layout(tile.(s.to_swap)) * literal("\n") *
-#     indent(4) * literal("Current best -> ") * list_layout(tile.(s.current_best)) *  literal("\n") *
-#     indent(4) * literal("To swap with -> ") * list_layout(tile.(s.to_swap_with)) * literal("\n");
-#     return ret_val;
-# end
-
-# tile(s::Swappable) = 
-#     if isempty(s.swapped)
-#         _all_but_swapped(s);
-#     else
-#         _all_but_swapped(s) * indent(4) * literal("Swapped      -> ") * list_layout(tile.(s.swapped)) 
-#     end
-
-# Base.show(io::IO,::MIME"text/plain", s::Swappable) = pprint(s)
-
+show(io::IO, s::Swap) = print(io, 
+"""
+Swap 
+    new: $(s.new)
+    obj_value: $(s.obj_value)          
+    success: $(s.success)            
+    all_fixed: $(s.all_fixed)          
+    termination_status: $(s.termination_status) 
+    solve_time: $(s.solve_time)
+""")
+show(io::IO, s::Swapper) = print(io, 
+"""
+Swapper
+    $(length(s.to_swap)) left to swap
+    Completed $(length(s.completed_swaps)) swaps
+    Sense $(s.sense)
+    Number of swaps: $(s.number_of_swaps)
+    Best Objective: $(best_objective(swapper))
+""")
