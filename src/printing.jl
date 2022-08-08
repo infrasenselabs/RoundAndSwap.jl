0b8ce106016c75b87a388da88d8c344046b696e2
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-08-08 12:10:26 +0100

    improve print messages for Swap and Swapper

--- src/printing.jl
+++ src/printing.jl
@@ -3,19 +3,22 @@ import Base.show
 show(io::IO, s::Swap) = print(io, 
 """
 Swap 
-    new: $(s.new)
-    obj_value: $(s.obj_value)          
-    success: $(s.success)            
-    all_fixed: $(s.all_fixed)          
+    existing:           $(s.existing)
+    new:                $(s.new)
+    obj_value:          $(s.obj_value)          
+    success:            $(s.success)            
+    all_fixed:          $(s.all_fixed)          
     termination_status: $(s.termination_status) 
-    solve_time: $(s.solve_time)
+    solve_time:         $(s.solve_time)
 """)
 show(io::IO, s::Swapper) = print(io, 
 """
 Swapper
     $(length(s.to_swap)) left to swap
-    Completed $(length(s.completed_swaps)) swaps
     Sense $(s.sense)
-    Number of swaps: $(s.number_of_swaps)
-    Best Objective: $(best_objective(swapper))
+    Best Objective: $(best_objective(s))
+    Completed $(s.number_of_swaps) swaps
+    $(length(successful_swaps(s))) were successful
+    $(length(unsuccessful_swaps(s))) were unsuccessful
+    The rest were be skipped
 """)
