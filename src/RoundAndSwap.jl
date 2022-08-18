46799e479d40be29cdc62a374554d973bf10a98b
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-08-18 12:17:20 +0100

    Add inout file, the methods are pretty ugly but they work

--- src/RoundAndSwap.jl
+++ src/RoundAndSwap.jl
@@ -5,6 +5,7 @@ include("swapping.jl")
 include("misc.jl")
 include("model.jl")
 include("printing.jl")
+include("inout.jl")
 
 # misc
 export flatten
@@ -13,8 +14,10 @@ export successful, fixed_variables, unfix!, make_models, fix!, get_var
 # printing
 
 # structs
-export Swap, Swapper, successful_swaps, unsuccessful_swaps, total_optimisation_time, num_swaps,status_codes
+export Swap, Swapper, successful_swaps, unsuccessful_swaps, total_optimisation_time, num_swaps,status_codes, RSStatusCodes
 # swapping
 export best_swap, previously_tried, best_objective, try_swapping!, initial_swaps, create_swaps!, evalute_sweep, round_and_swap
+# inout
+export save, load_swapper
 
 end # module RoundAndSwap
