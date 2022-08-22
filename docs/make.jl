cc4c85dd341ced2e8f45a46abcbfc36232e5efcc
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-08-22 12:54:00 +0100

    provide dev branch

--- docs/make.jl
+++ docs/make.jl
@@ -12,5 +12,6 @@ deploydocs(
     repo = "github.com/this-josh/RoundAndSwap.jl.git",
     push_preview = true,
     forcepush = true,
-    versions = ["stable" => "v^", "v#.#", devurl => "dev"]
+    devbranch="main",
+    versions = ["stable" => "v^", "v#.#.#"]
 )
