a1055fc19c4d9051cea12aa2b34b908ddd32223c
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-08-23 16:13:26 +0100

    add a check to prevent rounding too many

--- src/round.jl
+++ src/round.jl
@@ -4,6 +4,10 @@ function round!(to_consider::Vector{VariableRef}, num_to_fix::Int)
     thresh = minimum(sort(value.(to_consider))[end-num_to_fix+1:end])
     idx_to_fix = findall(x-> thresh≤x, value.(to_consider))
     variables_to_fix = to_consider[idx_to_fix]
+    
+    if length(variables_to_fix) != num_to_fix
+        error("It seems like there are multiple values at the rounding threshold.")
+    end
     @info "Fixing: " variables_to_fix
     fix.(variables_to_fix, 1; force=true)
 end
