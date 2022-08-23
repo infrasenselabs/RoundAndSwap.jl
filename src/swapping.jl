d1f135aea7c7c334330b12a2061a0211ebb98de5
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-08-23 16:29:33 +0100

    Added docstring

--- src/swapping.jl
+++ src/swapping.jl
@@ -223,6 +223,14 @@ function swap(
     end
     return swap(models, swapper)
 end
+
+"""
+    swap(models::Array{Model}, swapper::Swapper)
+
+# Arguments:
+- `models`: An array of models, one for each thread
+- `swapper`: An already initialised swapper, this can either be clean or it can be partially complete
+"""
 function swap(models::Array{Model}, swapper::Swapper)
     swapper._stop = false
     start_time = now()
