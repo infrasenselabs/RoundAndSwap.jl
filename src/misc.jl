dd7203075fa81c0211f8a32a346e13e1e75215c9
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-08-30 18:43:10 +0100

    save and load mean stats, need custom parsing

--- src/misc.jl
+++ src/misc.jl
@@ -1,3 +1,5 @@
+using OnlineStats
+
 """
     flatten(to_flatten::Array{Array{Swap}})
 
@@ -6,3 +8,11 @@ Flatten nested arrays of swaps
 function flatten(to_flatten::Array{Array{Swap}})
     return collect(Iterators.flatten(to_flatten))
 end
+
+
+function init_mean(vals)
+    m.μ = vals[:mean]
+    m.n = vals[:n]
+    return m
+end
+
