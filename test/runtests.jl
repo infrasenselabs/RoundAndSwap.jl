a7572cbfaf0625c0c52aa4ce5b7e6013f050862b
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-09-02 14:30:26 +0100

    correct test order

--- test/runtests.jl
+++ test/runtests.jl
@@ -39,9 +39,6 @@ _best_swap = _best_swap[1]
 @test length(swapper.completed_swaps) == 5
 @test num_swaps(swapper) == 6
 
-@test_throws OptimizeNotCalled objective_value(model)
-reproduce_best!(_best_swap, swapper, model)
-@test objective_value(model) == 10
 
 # Test restarting
 _, _short_swapper = swap(model, consider_swapping; max_swaps=3)
@@ -57,6 +54,10 @@ for f in ("swapper_in_loop.json", "test_swapper.json")
     _swapper = load_swapper(f)
     @test swapper == _swapper
 end
+
+@test_throws OptimizeNotCalled objective_value(model)
+reproduce_best!(_best_swap, swapper, model)
+@test objective_value(model) == 10
 
 # Test Min
 @objective(model, Min, (a[1] + b) + (2 * (b + c)) + (3 * (c - d)) + (4 * (d + a[1])))
